cebea4df90a5004d53bf62ad42775d7469c86ee2
Marcos Carvalho <marcospaulocs633@gmail.com>
2023-08-28 23:26:28 -0300

    first commit

---
+++ src/Components/slideStories.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const slideStories = () => {
+  return (
+    <div id='slide'>
+      <div id='slide-elements'>
+
+      </div>
+    </div>
+  )
+}
+
+export default slideStories
